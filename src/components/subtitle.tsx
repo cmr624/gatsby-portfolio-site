28dec97284f9155a3b374f290b98358ce74dec20
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-02-19 01:12:54 -0500

    upload

---
+++ src/components/subtitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+interface Props {
+
+}
+export const Subtitle : React.FC<Props> = () => {
+    return (<h3>
+        Hello, Typescript
+    </h3>)
+} 
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,11 +3,13 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Subtitle } from "../components/subtitle"
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
+    <Subtitle/>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
