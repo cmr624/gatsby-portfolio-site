035a72f85a77043f04df91c2986e429bee1f1bc3
Carlos Michael Rodriguez <cmr624@nyu.edu>
2020-03-08 13:39:08 -0400

    git adding contact page. never forget to git add. never

---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import CenteredTitle from "../components/centeredTitle"
+
+const Contact = () => (
+  <Layout>
+      <CenteredTitle
+      h1Content="Contact Me"
+      pContent="Please fill out the form below! If you don't hear back from me, feel free to email me at cm@nyu.edu!"
+      />
+      
+  </Layout>
+)
+
+export default Contact;
